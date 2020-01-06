7cb9146da5574383afd52f64fb75b9bd5237158b
Leta <leta@berkeley.edu>
2020-01-06 15:02:05 -0800

    update free speech child theme to match what was on wbe server

--- bk_freespeech_child/functions.php
+++ bk_freespeech_child/functions.php
@@ -1,5 +1,9 @@
 <?php
 function my_theme_enqueue_styles() {
+    //wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    //wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', $berkeley_brand['Version'], 'all' ); 
+    //wp_enqueue_style( 'entypo', get_template_directory_uri() . '/css/entypo.css', $berkeley_brand['Version'], 'all' );    
+
     $parent_style = 'berkeley_brand'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
@@ -7,10 +11,11 @@ function my_theme_enqueue_styles() {
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
-    );
- 
+    );    
 }
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
 
 /*REQUIRE AUTHENTICATION FOR REST API																	*/
 /*whitelist localhost																							*/
--- bk_freespeech_child/templates/parts/menu-main.php
+++ bk_freespeech_child/templates/parts/menu-main.php
@@ -2,38 +2,38 @@
 /**
  * The template used to load the Main Menu in header*.php
  *
- * @package Alien Ship
- * @since Alien Ship 0.70
+ * @since 2.0
+ * @package Berkeley Brand
+ *
  */
 ?>
 <!-- Main menu -->
-<div class="<?php echo apply_filters( 'alienship_main_navbar_class' , 'navbar navbar-default navbar-static-top yamm' ); ?>" role="navigation">
-	<div class="container">
-         	 <a href="http://berkeley.edu" class="home-link">UC Berkeley</a>           
-<?php		do_action( 'alienship_site_title' );
-			//do_action( 'alienship_site_description' ); ?>
-            
+<div class="<?php echo apply_filters( 'berkeley_brand_main_navbar_class' , 'navbar navbar-default navbar-static-top yamm' ); ?>" role="navigation">
+				<div class="container">
+					<?php get_template_part( '/templates/parts/header', 'wordmark' ); ?>
+
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex2-collapse">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<?php if (of_get_option('alienship_name_in_navbar',1) ) { ?>
-				<a class="navbar-brand" href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a>
-			<?php } ?>
 		</div>
 
 		<div class="collapse navbar-collapse navbar-ex2-collapse" id="main-nav">
 			<div id="tools-nav">
 				<div class="container">
-				
-					 <?php // get_search_form(); ?> 
+
+					<?php
+                    if ( of_get_option( 'berkeley_brand_search_bar', '1' ) ) {
+                        get_search_form();
+                    }
+                    ?>
 
 				</div>
 	      	</div>
-			<nav id="primary-nav" role="navigation">
-			<?php 
+			<nav id="primary-nav">
+			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'main',
 				'depth'          => 2,
